6fd9bd8a78ffef5999e137e0aa3406d705acb08f
Prince zala <princezala63@gmail.com>
2026-03-16 00:08:41 +0530

    update

---
+++ Lecture 1/L1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> // By this, we don't need to include every library files separately
+// Bcz it includes most of the libraries inside it. But it's not prefered to use it
+// bcz it take too much compilation time. So we should include only that header files which will be needed.
+using namespace std;
+int main()
+{
+    int a;       // 4 bytes.
+    long b;      // 4 bytes. (It's same as int)
+    long long c; // 8 bytes. (If we need to store number out of range of int)
+
+    float d;       // 4 bytes.
+    double e;      // 8 bytes.
+    long double f; // 10 bytes.
+
+    char g; // 1 byte.
+    string h; // 
+
+    return 0;
+}
